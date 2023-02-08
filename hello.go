7d83fd4d515e5e2e27031c08bea0657960d339b1
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-08 23:46:35 +0900

    feat: add lang case

--- hello.go
+++ hello.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
+const langSpanish = "Spanish"
+const langFrench = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == langSpanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == langFrench {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
