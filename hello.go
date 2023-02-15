99549d8782a9a813ab7aed2d5d0ba93709b05db4
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-15 09:59:34 +0900

    refactor: use named return
    Refs: https://andmorefine.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world#atohitotsunirifakut

--- hello.go
+++ hello.go
@@ -4,7 +4,6 @@ import "fmt"
 
 const spanish = "Spanish"
 const french = "French"
-const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
@@ -13,16 +12,17 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
