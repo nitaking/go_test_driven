16076a5756a57dce4e2bbbd40ab7a73fd0bb8572
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-19 12:06:10 +0900

    feat: create Perimeter

---
+++ perimeter/perimeter_test.go
@@ -0,0 +1,14 @@
+package perimeter
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := Perimeter(10.0, 10.0)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %2f", got, want)
+		}
+	})
+}
