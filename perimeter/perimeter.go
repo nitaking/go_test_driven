d05c5f4c1ee742e50b811596b8b68051a169d8a2
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-19 12:31:49 +0900

    refactor(struct): using interface

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -15,14 +15,14 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
-}
-
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Shape interface {
+	Area() float64
 }
