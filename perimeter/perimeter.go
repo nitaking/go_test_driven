3e8137dea2ffec0671753be8de48a226a2571e61
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-25 16:08:45 +0900

    feat: add triagnle for perimeter

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -2,6 +2,10 @@ package perimeter
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -23,6 +27,11 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-type Shape interface {
-	Area() float64
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -16,24 +16,34 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
 		{
+			name:  "Rectangle",
 			shape: Rectangle{12, 6},
 			want:  72,
 		},
 		{
+			name:  "Circle",
 			shape: Circle{10},
 			want:  314.1592653589793,
+		},
+		{
+			name:  "Triangle",
+			shape: Triangle{12, 6},
+			want:  36.0,
 		},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want %g", got, tt.want)
+			}
+		})
 	}
 
 }
