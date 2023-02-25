b34d47b52dba6ec1f130dd8f89b93c1cb0c892c6
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-25 16:20:10 +0900

    refactor: changed int to Bitcoin type

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,15 +2,17 @@ package wallet
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() interface{} {
+func (w *Wallet) Balance() Bitcoin {
 	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
-	return w.balance
+	return (*w).balance
 }
