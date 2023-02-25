d3dc1d666c2a7858294ac93654ff13604e1fe4df
nitaking <10850034+nitaking@users.noreply.github.com>
2023-02-25 16:16:52 +0900

    feat!: failed wallet testing
    - because not understand pointer

---
+++ wallet/wallet.go
@@ -0,0 +1,16 @@
+package wallet
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Deposit(amount int) {
+	w.balance += amount
+}
+
+func (w Wallet) Balance() interface{} {
+	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
+	return w.balance
+}
